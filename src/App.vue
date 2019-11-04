ec1fc4c0cf8e73e8229f73348a0c8204a16382ba
GeorgePap <gipap90@gmail.com>
2019-11-04 19:56:59 +0200

    feat(characters): added components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+      <NavBar />
+      <Characters />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/navbar';
+import Characters from '@/components/characters';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: { NavBar, Characters },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
