90d38a67c2e3134322de70b17fabc40ed253f82d
GeorgePap <gipap90@gmail.com>
2019-11-19 19:15:57 +0200

    added new vue components

---
+++ src/views/characters.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm" v-for="character in characters" :key="character.id">
+          <div>
+            <b-card
+              v-bind:title="character.name"
+              :img-src="character.url"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem;"
+              class="mb-2"
+            >
+              <b-card-text>{{character.description}}</b-card-text>
+
+              <b-button @click="ok" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Json from '../db.json';
+export default {
+  name: "Characters",
+  data() {
+    return {
+      characters: Json.characters
+    };
+  },
+  methods: {
+    ok() {
+        alert();
+    }
+  }
+};
+</script>
---
+++ src/views/map.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="map">
+    <img src="../assets/map.jpg" style="height:auto;width:100%" />
+  </div>
+</template>
